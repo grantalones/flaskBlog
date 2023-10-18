bb45d011829f534f4413ddb577c857236d594b04
Grant Pemberton <gpemberton@nuvasive.com>
2023-10-18 13:51:55 -0700

    adds some mongo functionality to render_template

--- app.py
+++ app.py
@@ -6,23 +6,26 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://gpnuvasive:gpnuvasivepassword@flaskblog.kwlaxrx.mongodb.net/")
 app.db = client.microBlog
 
-entries = []
-
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.insert(0, (entry_content, formatted_date))
+        # entries.insert(0, (entry_content, formatted_date))
         app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
         entries_with_date = [
-            (entry[0],
-             entry[1],
-             datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"))
-            for entry in entries
+            (entry["content"],
+             entry["date"],
+             datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"))
+            for entry in app.db.entries.find({})
         ]
-
-
+    if request.method == "GET":
+        entries_with_date = [
+            (entry["content"],
+             entry["date"],
+             datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"))
+            for entry in app.db.entries.find({})
+        ]
     return render_template("home.html", entries=entries_with_date)
