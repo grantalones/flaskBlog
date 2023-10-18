2d74bf22afe5b3c205f2ba91d7e3bfd7e010f4e7
Grant Pemberton <gpemberton@nuvasive.com>
2023-10-17 17:43:56 -0700

    adds MongoDB to app

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://gpnuvasive:gpnuvasivepassword@flaskblog.kwlaxrx.mongodb.net/")
+app.db = client.microBlog
+
+entries = []
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.insert(0, (entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (entry[0],
+             entry[1],
+             datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"))
+            for entry in entries
+        ]
+
+
+    return render_template("home.html", entries=entries_with_date)
